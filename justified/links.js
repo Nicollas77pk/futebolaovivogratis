38627851e66462f318f9c9a79fd08d2f63f74cba
futebol ao vivo <91692892+Nicollas77pk@users.noreply.github.com>
2025-04-24 10:15:32 -0300

    Create links.js

---
+++ justified/links.js
@@ -0,0 +1,38 @@
+const episodes = [
+  {
+    season: 1,
+    episode: 1,
+    title: "Justified S01E01",
+    url: "http://lexus.hubns.top:80/series/Leonardo77/983469871/3727722.mp4",
+    cover: "http://timg.bdta.pro/images/5b52e3be94b647488fef77b99973a8c1.jpg"
+  },
+  {
+    season: 1,
+    episode: 2,
+    title: "Justified S01E02",
+    url: "http://lexus.hubns.top:80/series/Leonardo77/983469871/3727723.mp4",
+    cover: "http://timg.bdta.pro/images/06592de5fd33db38910361c0905fe743.jpg"
+  },
+  {
+    season: 1,
+    episode: 3,
+    title: "Justified S01E03",
+    url: "http://lexus.hubns.top:80/series/Leonardo77/983469871/3727724.mp4",
+    cover: "http://timg.bdta.pro/images/39857dee06407e156c40ae6f944dd338.jpg"
+  },
+  {
+    season: 1,
+    episode: 4,
+    title: "Justified S01E04",
+    url: "http://lexus.hubns.top:80/series/Leonardo77/983469871/3727725.mp4",
+    cover: "http://timg.bdta.pro/images/41491cb9eb6fe9e01bc6d8ad8d5c5807.jpg"
+  },
+  {
+    season: 1,
+    episode: 12,
+    title: "Playlist [L] S01E12",
+    url: "http://lexus.hubns.top:80/series/Leonardo77/983469871/3727721.mp4",
+    cover: "" // Se tiver a imagem de capa, posso incluir
+  }
+];
+
