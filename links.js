ed0d5d73b15a641ee5629ecc83251c86974a4377
futebol ao vivo <91692892+Nicollas77pk@users.noreply.github.com>
2025-04-24 11:39:08 -0300

    Update links.js

--- links.js
+++ links.js
@@ -4,28 +4,28 @@ const episodes = [
     episode: 1,
     title: "Justified S01E01",
     url: "http://lexus.hubns.top:80/series/Leonardo77/983469871/3727722.mp4",
-    logo: "https://www.imdb.com/title/tt1522554/mediaviewer/rm1533748480/"
+    logo: "https://m.media-amazon.com/images/I/81kz03+7LCL._AC_SY679_.jpg"
   },
   {
     season: 1,
     episode: 2,
     title: "Justified S01E02",
     url: "http://lexus.hubns.top:80/series/Leonardo77/983469871/3727723.mp4",
-    logo: "https://www.rottentomatoes.com/tv/justified/s01/e02"
+    logo: "https://m.media-amazon.com/images/I/81kz03+7LCL._AC_SY679_.jpg"
   },
   {
     season: 1,
     episode: 3,
     title: "Justified S01E03",
     url: "http://lexus.hubns.top:80/series/Leonardo77/983469871/3727724.mp4",
-    logo: "https://justified.fandom.com/wiki/File:Fixer.jpg"
+    logo: "https://m.media-amazon.com/images/I/81kz03+7LCL._AC_SY679_.jpg"
   },
   {
     season: 1,
     episode: 4,
     title: "Justified S01E04",
     url: "http://lexus.hubns.top:80/series/Leonardo77/983469871/3727725.mp4",
-    logo: "https://en.wikipedia.org/wiki/File:Justified_S01E04.jpg"
+    logo: "https://m.media-amazon.com/images/I/81kz03+7LCL._AC_SY679_.jpg"
   }
 
 ];
