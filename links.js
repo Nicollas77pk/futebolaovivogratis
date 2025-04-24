82316eb7c117b9d1f031a0e19ffd8ce7680be1b3
futebol ao vivo <91692892+Nicollas77pk@users.noreply.github.com>
2025-04-24 10:47:14 -0300

    Update links.js

--- links.js
+++ links.js
@@ -2,42 +2,30 @@ const episodes = [
   {
     season: 1,
     episode: 1,
-    title: "Bem-vindo a Harlan",
-    url: "https://example.com/justified/s01e01.mp4",
-    logo: "https://upload.wikimedia.org/wikipedia/pt/7/70/Justified_Temporada_1_Poster.jpg"
+    title: "Justified S01E01",
+    url: "http://lexus.hubns.top:80/series/Leonardo77/983469871/3727722.mp4",
+    logo: "https://www.imdb.com/title/tt1522554/mediaviewer/rm1533748480/"
   },
   {
     season: 1,
     episode: 2,
-    title: "Rio Bravo",
-    url: "https://example.com/justified/s01e02.mp4",
-    logo: ""
+    title: "Justified S01E02",
+    url: "http://lexus.hubns.top:80/series/Leonardo77/983469871/3727723.mp4",
+    logo: "https://www.rottentomatoes.com/tv/justified/s01/e02"
   },
   {
     season: 1,
     episode: 3,
-    title: "Fixer",
-    url: "https://example.com/justified/s01e03.mp4"
+    title: "Justified S01E03",
+    url: "http://lexus.hubns.top:80/series/Leonardo77/983469871/3727724.mp4",
+    logo: "https://justified.fandom.com/wiki/File:Fixer.jpg"
   },
   {
-    season: 2,
-    episode: 1,
-    title: "O Martelo da Justiça",
-    url: "https://example.com/justified/s02e01.mp4",
-    logo: "https://upload.wikimedia.org/wikipedia/pt/5/56/Justified_season_2.jpg"
-  },
-  {
-    season: 2,
-    episode: 2,
-    title: "Blood or Money",
-    url: "https://example.com/justified/s02e02.mp4"
-    // Sem logo
-  },
-  {
-    season: 2,
-    episode: 3,
-    title: "Ouro e Sangue",
-    url: "https://example.com/justified/s02e03.mp4"
+    season: 1,
+    episode: 4,
+    title: "Justified S01E04",
+    url: "http://lexus.hubns.top:80/series/Leonardo77/983469871/3727725.mp4",
+    logo: "https://en.wikipedia.org/wiki/File:Justified_S01E04.jpg"
   }
-  // Adicione mais episódios conforme necessário
+
 ];
