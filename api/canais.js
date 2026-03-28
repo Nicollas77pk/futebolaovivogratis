6d30b657302efa254380d1fa815d4bb1c4dd59c5
futebol ao vivo <91692892+Nicollas77pk@users.noreply.github.com>
2026-03-28 11:29:30 -0300

    Update canais.js

--- api/canais.js
+++ api/canais.js
@@ -4,7 +4,7 @@ module.exports = async (req, res) => {
 #EXTM3U
 #EXTINF:-1,Kuriakos Kids 
 #EXTIMG https://example.com/icon.png
-https://edg.meusfavoritos.net/exemple.php?id=cfdd67b70d6fbb4cda8eb0f8db77d6a5
+http://gbbrlive2.sambatech.com.br/liveevent/sbtabr_8fcdc5f0f8df8d4de56b22a2c6660470/livestream2/playlist.m3u8
 
 #EXTINF:-1,filme
 #EXTIMG https://example.com/icon.png
